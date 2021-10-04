51229e56cf285aa7d1c96cddd1fe1e4fce3448bd
erik2305 <59746108+erik2305@users.noreply.github.com>
2021-10-04 19:15:15 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Log in page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "Sign up page.";
+
+            return View();
+        }
+
+        public ActionResult Account()
+        {
+            ViewBag.Message = "User's profile page.";
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Page with the list of events";
+
+            return View();
+        }
+    }
+}
