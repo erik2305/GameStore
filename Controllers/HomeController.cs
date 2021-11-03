e8524c1e3a87c73e0463c28c25c8bd817c8ad1ec
petersaundersConestoga <78880664+petersaundersConestoga@users.noreply.github.com>
2021-11-03 15:33:45 -0400

    Revert "the login and the signup page are almost ready, just experiencing a few errors I'm trying to fix"
    This reverts commit 5ef820b6b415c28a7e539765c8407a86f8b9c27e.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,93 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.SqlClient;
-using GameStore.Models;
 
 namespace GameStore.Controllers
 {
     public class HomeController : Controller
     {
-        //SqlConnection con = new SqlConnection();
-        //SqlCommand com = new SqlCommand();
-        //SqlDataReader dr;
         public ActionResult Index()
         {
             return View();
         }
-        //void connectionString()
-        //{
-        //    con.ConnectionString = "data source=[DESKTOP-1SPI0CK\\SQLEXPRESS]; database=CVGS; integrated security=SSPI;";
-        //}
-      
-        public ActionResult Signup()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
-        public ActionResult Signup(UserRegistration U)
-        {
-            if (ModelState.IsValid)
-            {
-                using (CVGSEntities4 cv = new CVGSEntities4())
-                {
-                    cv.UserRegistrations.Add(U);
-                    cv.SaveChanges();
-                    ModelState.Clear();
-                    U = null;
-                    ViewBag.Message = "Successfully Registration Done";
-                }
-            }
-            return View(U);
-        }
 
         public ActionResult Login()
         {
+            ViewBag.Message = "Log in page.";
 
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(UserRegistration u)
+        public ActionResult Signup()
         {
-            if (ModelState.IsValid)
-            {
-                using(CVGSEntities4 cv=new CVGSEntities4())
-                {
-                    var v = cv.UserRegistrations.Where(a => a.UserName.Equals(u.UserName) && a.Password.Equals(u.Password)).FirstOrDefault();
-                    if (v != null)
-                    {
-                        Session["LogedUserID"] = v.UserID.ToString();
-                        Session["LogedUserFullName"] = v.FirstName.ToString() + "" + v.LastName.ToString();
-                        return RedirectToAction("Events");
-                    }
-                }
-            }
-            return View(u);
+            ViewBag.Message = "Sign up page.";
 
+            return View();
         }
-        //public ActionResult AfterLogin()
-        //{
-        //    if (Session["LogedUserID"] != null)
-        //    {
-        //        return View();
-        //    }
-        //    else
-        //    {
-        //        return RedirectToAction("Index");
-        //    }
-           
-        //}
-
-        //public RedirectResult Signup()
-        //{
-        //    return Redirect("/Views/Home/Signup.aspx");
-        //}
-
 
         public ActionResult Account()
         {
