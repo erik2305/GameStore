f17d1bc7dbda72b16338460c5ab0e628642d4a0b
erik2305 <59746108+erik2305@users.noreply.github.com>
2021-11-04 16:23:00 -0400

    bebebe

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Models;
+
+namespace GameStore.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserRegistration U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CVGS_Tables cv = new CVGS_Tables())
+                {
+                    cv.UserRegistrations.Add(U);
+                    cv.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Successfully Registration Done";
+                }
+            }
+            return View(U);
+
+        }
+    }
+}
