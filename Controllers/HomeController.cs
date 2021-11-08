65b2f92d84efebb44e67029426625109b6894415
erik2305 <59746108+erik2305@users.noreply.github.com>
2021-11-08 16:32:07 -0500

    Login and Signup pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,33 +4,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GameStore.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace GameStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+            public ActionResult Index()
         {
             return View();
         }
 
         public ActionResult Login()
         {
-            ViewBag.Message = "Log in page.";
+            ViewBag.Message = "Log in page";
 
             return View();
         }
 
         public ActionResult Signup()
         {
-            ViewBag.Message = "Sign up page.";
-
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Person U)
+        {
+            if(ModelState.IsValid)
+            {
+                using (CVGS_Tables db = new CVGS_Tables())
+                {
+                    var check = db.People.FirstOrDefault(s => s.Email == U.Email);
+                    if (check == null)
+                    {
+                        U.Password = GetMD5(U.Password);
+                        db.Configuration.ValidateOnSaveEnabled = false;
+                        db.People.Add(U);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.error = "Email already exists";
+                        return View();
+                    }
+                }
+            }
+            return View();
+        }
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
 
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
         public ActionResult Account()
         {
-            ViewBag.Message = "User's profile page.";
+            ViewBag.Message = "User's profile page";
+            if (Session["userId"] == null)
+            {
+                TempData["Message"] = "Login to view your account info";
+                Response.Redirect("~/Home/Login");
+            }
 
             return View();
         }
@@ -47,6 +92,32 @@ namespace GameStore.Controllers
             ViewBag.Message = "Administration Portal";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(GameStore.Models.Person personModel)
+        {
+            using (CVGS_Tables db = new CVGS_Tables())
+            {
+                var userDetails = db.People.ToList().Where(x => (x.UserName == personModel.UserName && 
+                x.Password == personModel.Password)).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    personModel.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Login", personModel);
+                }
+                else
+                {
+                    Session["userId"] = userDetails.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -11,6 +11,8 @@ namespace GameStore.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Person
     {
@@ -26,9 +28,21 @@ namespace GameStore.Models
         public string Extension { get; set; }
         public string Mobile { get; set; }
         public string Fax { get; set; }
+        [Required(ErrorMessage = "Input Email", AllowEmptyStrings = false)]
         public string Email { get; set; }
+        [DisplayName("User Name")]
+        [Required(ErrorMessage ="Input User name", AllowEmptyStrings = false)]
         public string UserName { get; set; }
-    
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Input Password", AllowEmptyStrings = false)]
+        [StringLength(30, MinimumLength = 8, ErrorMessage ="Password must be at least 8 characters long")]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage ="Passwords doesn't match")]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        public string LoginErrorMessage { get; set; }
+
         public virtual Country Country { get; set; }
         public virtual Employee Employee { get; set; }
         public virtual Province Province { get; set; }
